2c37244b8816d953882fdba2a125cb266ebc50bc
渡劫 <2545251075@qq.com>
2021-05-08 16:47:51 +0800

    zuul 注册中心主动下线

--- common/src/main/java/com/js/controller/BaseController.java
+++ common/src/main/java/com/js/controller/BaseController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BaseController {
 
+//    http://localhost:9001/eureka/apps/OPERATION-SERVICE/10.238.183.28:operation-service:8080
     @GetMapping(value = "/offline")
     public void offLine() {
         DiscoveryManager.getInstance().shutdownComponent();
