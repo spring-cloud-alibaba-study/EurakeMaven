58b1356cddafa0efac229b7ff4f2839878962c81
渡劫 <2545251075@qq.com>
2021-05-08 09:41:44 +0800

    zuul 网关配置

--- gateway-service/src/main/java/com/js/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/js/GatewayServiceApplication.java
@@ -4,10 +4,11 @@ import com.js.util.StartLogo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @Slf4j
-
+@EnableZuulProxy
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
